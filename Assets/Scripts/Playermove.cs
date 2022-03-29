22a40709d888a25336b4c53ab52141185fcf3dbc
Ben S <dragonfart7768@gmail.com>
2022-03-28 17:04:58 -0700

    lesson 3.28.2022

--- Assets/Scripts/Playermove.cs
+++ Assets/Scripts/Playermove.cs
@@ -7,23 +7,29 @@ public class Playermove : MonoBehaviour
     public float MoveSpeed = 5f;
     public float JumpForce = 10f;
     public Rigidbody2D rb;
+    public Collider2D collider;
+    public Collider2D feet;
+    public int maxJumps = 2;
+    private int jumpsLeft;
     // Start is called before the first frame update
     void Start()
     {
-        
+        jumpsLeft = maxJumps;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A)){
-            rb.velocity = new Vector2(-MoveSpeed, rb.velocity.Y);
+        if (Input.GetKeyDown(KeyCode.Space) && jumpsLeft > 0){
+            rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+            jumpsLeft -= 1;
         }
-        if (Input.GetKeyDown(KeyCode.Space)){
-            rb.velocity = new Vector2(rb.velocity.X, JumpForce);
-        }
-        if (Input.GetKeyDown(KeyCode.D)){
-            rb.velocity = new Vector2(MoveSpeed, rb.velocity.Y);
-        }
+        float inputX = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(MoveSpeed*inputX, rb.velocity.y, 0);
+        transform.Translate(direction*Time.deltaTime);
+    }
+    void OnCollisionEnter(Collision c){
+        jumpsLeft = maxJumps;
+        Debug.Log("bgcrfeuvbreicfbnw");
     }
 }
